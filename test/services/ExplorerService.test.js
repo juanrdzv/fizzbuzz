112f04bb0821f69f4d915ac701e9693d13e25889
Juan Rodriguez Villalvazo <juanrdzvzo@gmail.com>
2022-05-10 11:59:13 -0500

    Agregando

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -12,15 +12,15 @@ describe("Tests para ExplorerService", () => {
 
 describe("2 Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular el numero de explorers en la mision node", () =>{
-        const explorers = Reader.readJsonfile("explorers.json")
-        const explorersNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        const explorers = Reader.readJsonfile("explorers.json");
+        const explorersNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersNode).toBe(10);
     });
 });
 describe("3 Tests para ExplorerService", () => {
     test("Requerimiento 1: Obtener nombres de los explorers en Mission: Node", () =>{
-        const explorers = Reader.readJsonfile("explorers.json")
-        const explorersNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        const explorers = Reader.readJsonfile("explorers.json");
+        const explorersNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
         expect(explorersNode).not.toBeUndefined;
     });
 });
